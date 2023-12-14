1177f934c7f628777dea5971e7ac7a94ca46e345
Tahsin Ahmed Chowdhury <39807939+tchowdhu@users.noreply.github.com>
2023-12-14 13:19:58 -0800

    Update ncs540-ztp.py

--- ncs540-ztp.py
+++ ncs540-ztp.py
@@ -15,6 +15,7 @@ reserved. Unless required by applicable law or agreed to separately in
 writing, software distributed under the License is distributed on an "AS
 IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 or implied.
+
 '''
 
 import  os
